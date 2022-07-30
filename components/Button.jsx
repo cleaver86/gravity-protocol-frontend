dc506cfd1a4f945e5556b58b31ed055e1d19b4be
cleaver86 <cory.cleaver@gmail.com>
2022-07-30 14:57:48 -0700

    test

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { styled } from '@mui/material/styles'
 import MaterialButton from '@mui/material/Button'
 
-const StyledButton = styled(MaterialButton)``
+const StyledButton = styled(MaterialButton)`    `
 
 export default function Button() {
   return <StyledButton variant="contained">Contained</StyledButton>
