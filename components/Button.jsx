7330c8b90649f497394480659adf6e1eb5d8a7ca
cleaver86 <cory.cleaver@gmail.com>
2022-07-30 14:06:36 -0700

    Added base dependencies for next and storybook. Added Material.

---
+++ components/Button.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import MaterialButton from '@mui/material/Button';
+
+
+const StyledButton = styled(MaterialButton)`
+  
+`;
+
+export default function Button() {
+  return (
+    <StyledButton variant="contained">Contained</StyledButton>
+  );
+}
